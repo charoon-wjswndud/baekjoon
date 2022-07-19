df919193941b86cb5508f5e63773fb811a221eca
charoon <55038376+charoon-wjswndud@users.noreply.github.com>
2022-07-19 17:50:56 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		
+		System.out.println(name.concat("??!"));
+	}
+}
