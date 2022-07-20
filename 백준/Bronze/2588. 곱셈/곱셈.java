6dc05f6988dd0fb6b9742987262ce6bb2e75535e
charoon <55038376+charoon-wjswndud@users.noreply.github.com>
2022-07-20 22:04:43 +0900

    [Bronze III] Title: 곱셈, Time: 208 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int B100 = B/100;
+		int B10 = (B%100-B%10)/10;
+		int B1 = B%10;
+		System.out.println(A*B1);
+		System.out.println(A*B10);
+		System.out.println(A*B100);
+		System.out.println(A*B);
+	}
+}
