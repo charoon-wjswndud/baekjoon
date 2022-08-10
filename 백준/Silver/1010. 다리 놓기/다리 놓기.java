d9979e98de632d7f6e11452d3b340b053509f72f
charoon <55038376+charoon-wjswndud@users.noreply.github.com>
2022-08-10 16:43:00 +0900

    [Silver V] Title: 다리 놓기, Time: 84 ms, Memory: 11856 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,24 +1,29 @@
-import java.util.Scanner;
-public class Main{
-	static int[][] dp = new int[30][30];
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[][] pascalTriangle = new int[30][30];
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < T; i++) {
-			int N = sc.nextInt();
-			int M = sc.nextInt();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
 			sb.append(combi(M, N)).append('\n');
 		}
 		System.out.println(sb);
 	}
 	static int combi(int n, int r) {
-		if(dp[n][r] > 0) {
-			return dp[n][r]; 
+		if(pascalTriangle[n][r] > 0) {
+			return pascalTriangle[n][r]; 
 		}
 		if(n == r || r == 0) {
-			return dp[n][r] = 1;
+			return pascalTriangle[n][r] = 1;
 		}
-		return dp[n][r] = combi(n - 1, r - 1) + combi(n - 1, r);
+		return pascalTriangle[n][r] = combi(n - 1, r - 1) + combi(n - 1, r);
 	}
 }
