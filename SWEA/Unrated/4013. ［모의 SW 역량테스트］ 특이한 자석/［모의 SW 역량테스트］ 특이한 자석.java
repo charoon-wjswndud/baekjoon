a46060b52636dd6250306a4b37f0a58144901740
charoon <55038376+charoon-wjswndud@users.noreply.github.com>
2022-09-11 23:39:29 +0900

    [Unrated] Title: [모의 SW 역량테스트] 특이한 자석, Time: 107 ms, Memory: 19,164 KB -BaekjoonHub

--- SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
+++ SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
@@ -1,88 +1,102 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
-/*
- * 1. 자석 정보 입력
- * 2. 회전방향 배열에 저장
- * 3. 기준이 되는 자석의 오른쪽, 왼쪽 검사
- * 4. 회전
- * 5. 점수 연산
- */
-public class Solution {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		for(int testCase=1; testCase<=T; testCase++) {
-			
-			//자석 정보 2차원 배열에 입력
-			int K = Integer.parseInt(br.readLine());
-			int[][] info = new int[4][8];
-			for(int i=0; i<4; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine());
-				for(int j=0; j<8; j++) {
-					info[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-			
-            //K번 반복
-			for(int k=0; k<K; k++) {
-				StringTokenizer st = new StringTokenizer(br.readLine());
-				int num = Integer.parseInt(st.nextToken())-1;
-				int direction = Integer.parseInt(st.nextToken());
-                
-				int[] rotation = new int[4]; //방향 저장
-				rotation[num] = direction;
-				
-                //오른쪽 확인
-				for(int j=num+1; j<4; j++) {
-					if(info[j-1][2] == info[j][6]) //자성이 같으면 회전하지 않는다.
-						break;
-					else
-						rotation[j] = -rotation[j-1];
-				}
-				
-                //왼쪽 확인
-				for(int j=num-1; j>=0; j--) {
-					if(info[j][2] == info[j+1][6]) 
-						break;
-					else
-						rotation[j] = -rotation[j+1];
-				}
-				
-				//회전
-				for(int j=0; j<4; j++) {
-					if(rotation[j] == 0) continue;
-					else if(rotation[j] == 1) { 
-						int tmp = info[j][7];
-						for(int l=7; l>0; l--) {
-							info[j][l] = info[j][l-1];
-						}
-						info[j][0] = tmp;
-					} else if(rotation[j] == -1) { 
-						int tmp = info[j][0];
-						for(int l=0; l<7; l++) {
-							info[j][l] = info[j][l+1];
-						}
-						info[j][7] = tmp;
-					}
-				}
-				
-			}
-			
-			int sum = 0;
-			for(int i=0; i<4; i++) {
-				if(info[i][0] == 1) {
-					sum += 1<<i;
-				}
-			}
-			
-			System.out.println("#"+testCase+" "+sum);
-		}
-	}
 
+public class Solution {
+    static final boolean N = false; // 0
+    static final boolean S = true;  // 1
+    static final int ANTICLOCKWISE = -1, LEFT = -1;
+    static final int CLOCKWISE = 1, RIGHT = 1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int testCase = 1; testCase <= T; testCase++){
+            sb.append("#").append(testCase).append(" ");
+
+            CogWheel[] CogWheels = new CogWheel[4];
+
+            int turnNum = Integer.parseInt(br.readLine());
+            setCogWheel(CogWheels, br);
+
+            for (int i = 0; i < turnNum; i++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int target = Integer.parseInt(st.nextToken())-1;
+                int direction = Integer.parseInt(st.nextToken());
+                int[] rotation = new int[4];
+                rotateAble(rotation, CogWheels, target, direction);
+                turn(rotation, CogWheels);
+            }
+
+            int score = calcScore(CogWheels);
+            sb.append(score).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static int calcScore(CogWheel[] cogWheels) {
+        int score = 0;
+        for (int i = 0; i < 4; i++){
+            if(cogWheels[i].sawTeeth.get(0) == S) score += Math.pow(2, i);
+        }
+        return score;
+    }
+
+    private static void turn(int[] rotation, CogWheel[] cogWheels) {
+        for (int i = 0; i < 4; i++){
+            int direction = rotation[i];
+            List<Boolean> now = cogWheels[i].sawTeeth;
+            if(direction != 0){
+                boolean temp;
+                switch (direction){
+                    case CLOCKWISE:
+                        temp = now.get(7);
+                        now.remove(7);
+                        now.add(0, temp);
+                        break;
+                    case ANTICLOCKWISE:
+                        temp = now.get(0);
+                        now.remove(0);
+                        now.add(7, temp);
+                        break;
+                }
+            }
+        }
+    }
+
+    private static void rotateAble(int[] rotation, CogWheel[] cogWheels, int target, int direction) {
+        rotation[target] = direction;
+
+        for (int i = 0; i < 2; i++){
+            int next = target + (i == 0? LEFT : RIGHT);
+            List<Boolean> nowSawTeeth = cogWheels[target].sawTeeth;
+            if(0 <= next && next < 4 && rotation[next] == 0){   //범위확인, 방문확인
+                List<Boolean> nextSawTeeth = cogWheels[next].sawTeeth;
+                if(nowSawTeeth.get((i == 0)? 6 : 2) != nextSawTeeth.get((i == 0)? 2 : 6)){  //next회전 확인
+                    rotateAble(rotation, cogWheels, next, ((direction == 1)? -1 : 1));
+                }
+            }
+        }
+    }
+
+    private static void setCogWheel(CogWheel[] cogWheels, BufferedReader br) throws IOException {
+        for (int i = 0; i < 4; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            List<Boolean> sawTeeth = new ArrayList<>();
+            for (int j = 0; j < 8; j++){
+                sawTeeth.add((st.nextToken().equals("0")) ? N : S);
+            }
+            cogWheels[i] = new CogWheel(sawTeeth);
+        }
+    }
+
+    static class CogWheel{
+        List<Boolean> sawTeeth = new ArrayList<>();
+        public CogWheel(List<Boolean> sawTeeth) {
+            this.sawTeeth = sawTeeth;
+        }
+    }
 }
