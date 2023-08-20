c4e771351d6116cc459ffe86d03dd19f4cb4634e
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-08-20 22:29:27 +0900

    [Silver IV] Title: 제로, Time: 216 ms, Memory: 21252 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,26 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.*;
 
-public class Main {
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
+public class Main{
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		int N = Integer.parseInt(br.readLine());
-		
 		Stack<Integer> stack = new Stack<>();
 		for (int i = 0; i < N; i++) {
 			int num = Integer.parseInt(br.readLine());
-			if(num != 0) stack.add(num);
-			else stack.pop();
+			if (num == 0)
+				stack.pop();
+			else
+				stack.push(num);
 		}
-		int result = 0;
-		while(!stack.isEmpty()) {
-			result += stack.pop();
-		}
-		System.out.println(result);
-	}
 
+		int total = 0;
+		for (Integer num : stack)
+			total += num;
+
+		System.out.print(total);
+	}
 }
