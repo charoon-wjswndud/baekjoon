df49c29fcebeb7fe2f65f5715f2a60f3fef19509
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-08-25 19:17:56 +0900

    [Silver IV] Title: 설탕 배달, Time: 76 ms, Memory: 11708 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,21 +1,26 @@
-import java.util.Scanner;
-/*
- * 3	4	|5	6	7	8	9	|10	11	12	13	14	|15	16	17	18	19	|20	21	22	23	24
- * 1	-1	|1	2	-1	2	3	|2	3	4	3	4	|3	4	5	4	5	|4	5	6	5	6
- * 규칙성이 있다. 
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int count = 0;
-		if(N == 4 || N == 7) count = -1;
-		else if(N%5 == 0 ) count = N/5;
-		else if(N%5 == 1 || N%5 == 3) count = N/5 + 1;
-		else if(N%5 == 2 || N%5 == 4) count = N/5 + 2;
-		
-		System.out.println(count);
+		int[] dp = new int[50001];
+		dp[3] = 1;
+		dp[5] = 1;
+
+		for (int i = 6; i <= N; i++) {
+			if (i % 5 == 0)
+				dp[i] = dp[i-5]+1;
+			else if (i % 3 == 0)
+				dp[i] = dp[i-3]+1;
+			else
+				if (dp[i-5] != 0 && dp[i-3] != 0)
+					dp[i] = Math.max(dp[i-3], dp[i-5])+1;
+		}
+		System.out.println(dp[N]!=0?dp[N]:"-1");
 	}
-
 }
