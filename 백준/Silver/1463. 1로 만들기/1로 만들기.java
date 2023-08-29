b873a7793ed68e7ae8321a402335acc2d3429c6c
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-08-29 20:47:18 +0900

    [Silver III] Title: 1로 만들기, Time: 108 ms, Memory: 15800 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,21 +2,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//https://www.acmicpc.net/problem/1463
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int[] memoryArr = new int[(int)Math.pow(10, 6) + 1];
-
-        int N = Integer.parseInt(br.readLine());
-
-        for(int n = 2; n < N+1; n++){
-            if(n%3 == 0 && n%2 == 0) memoryArr[n] = Math.min(memoryArr[n/3] + 1, memoryArr[n/2]+1);
-            else if (n%3 == 0) memoryArr[n] = Math.min(memoryArr[n-1]+1,  memoryArr[n/3]+1);
-            else if (n%2 == 0) memoryArr[n] = Math.min(memoryArr[n-1]+1,  memoryArr[n/2]+1);
-            else memoryArr[n] = memoryArr[n-1] + 1;
-        }
-
-        System.out.println(memoryArr[N]);
-    }
+	static int[][] dp;
+	static StringBuilder sb;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[1_000_001];
+		dp[1] = 0;
+		for (int i = 2; i <= N; i++) {
+			if (i%2 != 0 && i%3 != 0)
+				dp[i] = dp[i-1]+1;
+			else if (i%2 == 0 && i%3 != 0)
+				dp[i] = Math.min(dp[i-1], dp[i/2]) + 1;
+			else if (i%2 != 0 && i%3 == 0)
+				dp[i] = Math.min(dp[i-1], dp[i/3]) + 1;
+			else
+				dp[i] = Math.min(dp[i-1], Math.min(dp[i/2], dp[i/3])) + 1;
+		}
+		System.out.println(dp[N]);
+	}
 }
+
