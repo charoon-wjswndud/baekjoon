23dd5492c1821216816caeddd25bc66f27d104d7
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-08-31 13:37:25 +0900

    [Silver III] Title: 2×n 타일링, Time: 76 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,16 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
-public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+public class Main{
+	static int[] dp = new int[1001];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 
-		int[] dp = new int[1001];
-		dp[1] = 1;
-		dp[2] = 2;
-		for (int i = 3; i <= n; i++)
-			dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
-		System.out.print(dp[n]);
+		if (N < 3)
+			System.out.println(N);
+		else
+			System.out.println(topdown(N));
+	}
 
+	private static int topdown(int n) {
+		if (n < 3)
+			return dp[n] = n;
+		else if (dp[n] == 0)
+			return dp[n] = (topdown(n-1) + topdown(n-2))%10007;
+		else
+			return dp[n];
 	}
 }
