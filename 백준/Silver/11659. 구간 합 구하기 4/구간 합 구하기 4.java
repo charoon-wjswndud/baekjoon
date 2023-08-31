47c4cc99b29dad3ec803d8a5bf10efccbb2fef8b
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-08-31 21:25:48 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 744 ms, Memory: 65520 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,37 +1,35 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
+	static int[] dp = new int[11];
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		int[] arr = new int[N];
-		st = new StringTokenizer(br.readLine());	
-		for (int i = 0; i < arr.length; i++) {
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++)
 			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		int[] sumArr = new int[N];
-		int sum = 0;
-		for (int i = 0; i < N; i++) {
-			sum += arr[i];
-			sumArr[i] = sum;
-		}
-		for (int index = 0; index < M; index++) {
+		int[] sum = new int[N];
+		sum[0] = arr[0];
+		for (int i = 1; i < N; i++)
+			sum[i] = arr[i] + sum[i-1];
+		StringBuilder sb = new StringBuilder();
+		for (int k = 0; k < M; k++) {
 			st = new StringTokenizer(br.readLine());
-			int i = Integer.parseInt(st.nextToken())-1;
+			int i = Integer.parseInt(st.nextToken())-2;
 			int j = Integer.parseInt(st.nextToken())-1;
-			if (i <= 0) bw.write(Integer.toString(sumArr[j])); 
-			else bw.write(Integer.toString(sumArr[j]-sumArr[i-1]));
-			bw.newLine();
+			int result = 0;
+			if (i == -1)
+				result = sum[j];
+			else
+				result = sum[j] - sum[i];
+			sb.append(result).append("\n");
 		}
-		bw.flush();
-		bw.close();
+		System.out.print(sb);
 	}
 }
