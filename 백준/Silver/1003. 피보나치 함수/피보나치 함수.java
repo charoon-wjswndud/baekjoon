94bda709be69d5c4dc46b936be88117895ad4f11
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-08-31 10:25:52 +0900

    [Silver III] Title: 피보나치 함수, Time: 76 ms, Memory: 11416 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,29 +1,33 @@
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static Integer[][] dp = new Integer[41][2];
+	static Point[] dp = new Point[41];
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		while (0 < T--) {
+			int N = Integer.parseInt(br.readLine());
+			fibo(N);
+			sb.append(dp[N].x).append(" ").append(dp[N].y).append("\n");
+		}
+		System.out.print(sb);
+	}
 
-    public static void main(String[] args) throws IOException {
-        dp[0][0] = 1;
-        dp[0][1] = 0;
-        dp[1][0] = 0;
-        dp[1][1] = 1;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        for (int t = 0; t < T; t++) {
-            int N = Integer.parseInt(br.readLine());
-            fibonacci(N);
-            System.out.println(dp[N][0] + " " + dp[N][1]);
-        }
-    }
-    private static Integer[] fibonacci(int n) {
-        if(dp[n][0] == null || dp[n][1] == null) {
-            dp[n][0] = fibonacci(n-1)[0] + fibonacci(n-2)[0];
-            dp[n][1] = fibonacci(n-1)[1] + fibonacci(n-2)[1];
-        }
-
-        return dp[n];
-    }
+	private static Point fibo(int n) {
+		if (n == 0)
+			return dp[0] = new Point(1, 0);
+		else if(n == 1)
+			return dp[1] = new Point(0, 1);
+		else if(dp[n] != null)
+			return dp[n];
+		else  {
+			Point p1 = fibo(n-1);
+			Point p2 = fibo(n-2);
+			return dp[n] = new Point(p1.x + p2.x, p1.y + p2.y);
+		}
+	}
 }
