f9f1b6ea54685e3333bb3baa1baa5ff3e0c57839
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-09-04 00:31:51 +0900

    [Gold V] Title: 토마토, Time: 544 ms, Memory: 124736 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,91 +1,86 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[] dy = {0, 1, 0, -1};
-	static int[] dx = {1, 0, -1, 0};
-	static int R;
-	static int C;
-	static int[][] box;
-	public static void main(String[] args)throws IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		C = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());
-		box = new int[R][C];
-		List<Tomato> ripeTomatoList = new ArrayList<>();
-		for (int row = 0; row < R; row++) {
-			st = new StringTokenizer(br.readLine());
-			for (int column = 0; column < C; column++) {
-				box[row][column] = Integer.parseInt(st.nextToken());
-				if(box[row][column] == 1) ripeTomatoList.add(new Tomato(row, column, 0));
-			}
-		}
-		int time = bfs(ripeTomatoList);
-		
-		if(searchUnripeTomato()) System.out.println(-1);
-		else System.out.println(time);
-	}
-	private static boolean searchUnripeTomato() {
-		for (int[] row : box) {
-			for (int column : row) {
-				if(column == 0) return true;
-			}
-		}
-		return false;
-	}
-	private static int bfs(List<Tomato> ripeTomatoList) {
-		Tomato lastTomato = null;
+		int X = Integer.parseInt(st.nextToken());
+		int Y = Integer.parseInt(st.nextToken());
+		boolean[][] map = new boolean[Y][X];
 		Queue<Tomato> queue = new LinkedList<>();
-		boolean[][] visited = new boolean[R][C];
-		for (Tomato ripeTomato : ripeTomatoList) {
-			queue.add(ripeTomato);
-			visited[ripeTomato.row][ripeTomato.column] = true;
+
+		boolean check = false;
+		for (int y = 0; y < Y; y++) {
+			st = new StringTokenizer(br.readLine());
+			for (int x = 0; x < X; x++) {
+				String state = st.nextToken();
+				if (state.equals("1")) {
+					queue.add(new Tomato(y, x));
+					map[y][x] = false;
+				} else if (state.equals("0")){
+					map[y][x] = true;
+					check = true;
+				} else
+					map[y][x] = false;
+			}
 		}
-		while(!queue.isEmpty()) {
-			Tomato now = queue.poll();
-			lastTomato = now;
-			//4방탐색
-			for (int direction = 0; direction < 4; direction++) {
-				if(isPossible(visited, now, direction)) {
-					int nextRow = now.row+dy[direction];
-					int nextColumn = now.column+dx[direction];
-					queue.add(new Tomato(now.row+dy[direction], now.column+dx[direction], now.time+1));
-					visited[nextRow][nextColumn] = true;
-					box[nextRow][nextColumn] = 1;
+		//저장될 때부터 모든 토마토가 익어있는 상태이면 0을 출력
+		if (!check) {
+			System.out.println(0);
+			return;
+		}
+
+		int[][] nd = {{0, 1},{1, 0},{0, -1},{-1, 0}};
+		Tomato last = null;
+		while (!queue.isEmpty()) {
+			last = queue.poll();
+			for (int i = 0; i < 4; i++) {
+				int ny = last.y + nd[i][0];
+				int nx = last.x + nd[i][1];
+				if (ny < 0 || Y <= ny || nx < 0 || X <= nx)
+					continue;
+				if (!map[ny][nx])
+					continue;
+				queue.add(new Tomato(ny, nx, last.day+1));
+				map[ny][nx] = false;
+			}
+		}
+		for (boolean[] b1: map){
+			for (boolean b2:b1) {
+				if (b2){
+					System.out.println("-1");
+					return;
 				}
 			}
 		}
-		return lastTomato != null ? lastTomato.time : 0;
+
+		System.out.println(last.day);
+
 	}
-	
-	private static boolean isPossible(boolean[][] visited, Tomato now, int direction) {
-		int nextRow = now.row + dy[direction];
-		int nextColumn = now.column + dx[direction];
-		if(0 <= nextRow && nextRow < R &&
-				0 <= nextColumn && nextColumn < C &&
-				box[nextRow][nextColumn] == 0 &&
-				!visited[nextRow][nextColumn]) {
-			return true;
-		}
-		return false;
-	}
+
 	static class Tomato{
-		int row;
-		int column;
-		int time;
-		public Tomato(int row, int column, int time) {
-			super();
-			this.row = row;
-			this.column = column;
-			this.time = time;
+		int y;
+		int x;
+		int day;
+		boolean state;
+
+		public Tomato(int y, int x) {
+			this.y = y;
+			this.x = x;
+			state = true;
+			day = 0;
+		}
+
+		public Tomato(int y, int x, int day) {
+			this.y = y;
+			this.x = x;
+			state = true;
+			this.day = day;
 		}
 	}
 }
