7dfe654fc9696a983067be1854c05616b1163b20
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-09-04 15:19:09 +0900

    [Silver I] Title: Z, Time: 80 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -4,51 +4,45 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int cnt = 0;
+	static int r, c;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
 
-		int num = 1 << Integer.parseInt(st.nextToken());
-		int r = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
-
-		divideArr(num, r, c);
-		System.out.print(cnt);
+		int result = DivideConquer(N, 0, 0, 0);
+		System.out.println(result);
 	}
 
-	private static void divideArr(int num, int r, int c) {
-		/*
-		┌─────┬─────┐
-		│ 0,0 │ 0,1 │
-		├─────┼─────┤
-		│ 1,0 │ 1,1 │
-		└─────┴─────┘
-		 */
-		if(num == 1)
-			return;
-
-		//0, 0
-		if(r < num/2 && c < num/2) {
-			divideArr(num/2, r, c);
+	private static int DivideConquer(int n, int y, int x, int cnt) {
+		if (n == 0) {
+			return cnt;
 		}
+		int num = (int) Math.pow(2, n)/2;
+		int quadrant = calcQuadrant(n, y, x);
+		if (quadrant == 1)
+			return DivideConquer(n-1, y, x+num, cnt + num*num);
+		else if ( quadrant == 2)
+			return DivideConquer(n-1, y, x, cnt);
+		else if ( quadrant == 3)
+			return DivideConquer(n-1, y+num, x, cnt+num*num*2);
+		else
+			return DivideConquer(n-1, y+num, x+num, cnt + num*num*3);
+	}
 
-		//0, 1
-		else if(r < num/2 && c >= num/2) {
-			cnt += num * num / 4;
-			divideArr(num/2, r, c - num/2);
-		}
-
-		//1, 0
-		else if(r >= num/2 && c < num/2) {
-			cnt += (num * num / 4) * 2;
-			divideArr(num/2, r - num/2, c);
-		}
-
-		//1, 1
-		else {
-			cnt += (num * num / 4) * 3;
-			divideArr(num/2, r - num/2, c - num/2);
-		}
+	private static int calcQuadrant(int n, int y, int x) {
+		int num =(int) Math.pow(2,n)/2;
+		if (r < y + num)
+			if (c < x + num)
+				return 2;
+			else
+				return 1;
+		else
+			if (c < x + num)
+				return 3;
+			else
+				return 4;
 	}
 }
