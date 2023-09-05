643f759839fd07c38b6a1f784f9ffc2181ea5bcd
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-09-05 15:32:20 +0900

    [Silver I] Title: 절댓값 힙, Time: 300 ms, Memory: 25324 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -4,38 +4,30 @@ import java.io.InputStreamReader;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
-public class Main {
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> que = new PriorityQueue<>(
-        		new Comparator<Integer>(){
-        			@Override
-        			public int compare(Integer o1, Integer o2) {
-        				return  Math.abs(o1) == Math.abs(o2) ? Integer.compare(o1, o2) : Integer.compare(Math.abs(o1), Math.abs(o2));
-        			}    			
-        		}
-        	);	
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
-
-            if (num == 0)
-                sb.append(que.size() == 0 ? 0 : que.poll()).append('\n');
-            else que.add(num);
-        }
-        System.out.println(sb.toString());
+public class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				if (Math.abs(o1) == Math.abs(o2))
+					return Integer.compare(o1, o2);
+				else
+					return Integer.compare(Math.abs(o1), Math.abs(o2));
+			}
+		});
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if (num == 0)
+				if (priorityQueue.isEmpty())
+					sb.append(0).append("\n");
+				else
+					sb.append(priorityQueue.poll()).append("\n");
+			else
+				priorityQueue.add(num);
+		}
+		System.out.print(sb);
 	}
 }
-
-class MyComparator implements Comparator<Integer>{
-	@Override
-	public int compare(Integer o1, Integer o2) {
-		return  Math.abs(o1) == Math.abs(o2) ? Integer.compare(o1, o2) : Integer.compare(Math.abs(o1), Math.abs(o2));
-	}
-	
-}
-
-
