e1810e7f3757a1e4e46f1feed023632ecceb2c22
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-09-11 12:44:21 +0900

    [Silver I] Title: IOIOI, Time: 172 ms, Memory: 22572 KB -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		char[] S = br.readLine().toCharArray();
+
+		int result = 0, cnt = 0;
+		for(int i=1; i < M - 1; i++) {
+			if((S[i-1] == 'I') && (S[i] == 'O') && (S[i+1] == 'I')) {
+				cnt++;
+				if(cnt == N) {
+					cnt--;
+					result++;
+				}
+				i++;
+			}
+			else 
+				cnt = 0;
+		}
+		System.out.println(result);
+	}
+}
