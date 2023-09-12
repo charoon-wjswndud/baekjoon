d6befd8de341c81fb9644d33224e1dc8460b09c9
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-09-12 14:46:56 +0900

    [Bronze V] Title: A/B, Time: 80 ms, Memory: 11600 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,14 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		
-		System.out.println((double)A/B);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		double A = Double.parseDouble(st.nextToken());
+		double B = Double.parseDouble(st.nextToken());
+		System.out.println(A/B);
 	}
-
 }
