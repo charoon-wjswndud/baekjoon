a2abc1bb4087947fa90a09d63c0f7b7a036087d4
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-09-15 13:00:32 +0900

    [Gold V] Title: 리모컨, Time: 352 ms, Memory: 100116 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -3,40 +3,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
 
-        //remoteController 고장난 button을 false
-        boolean[] remoteController = new boolean[10];
-        for (int i = 0; i < 10; i++)
-            remoteController[i] = true;
-        int M = Integer.parseInt(br.readLine());
-        if (M != 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int m = 0; m < M; m++)
-                remoteController[Integer.parseInt(st.nextToken())] = false;
-        }
+		boolean[] brokenButton = new boolean[10];
+		
+		StringTokenizer st = null;
+		if (M != 0)
+			st = new StringTokenizer(br.readLine());
+		for(int i = 0; i<M; i++) {
+			int button = Integer.parseInt(st.nextToken());
+			brokenButton[button] = true;
+		}
 
-        // 완전탐색,
-        // 채널의 최댓값은 600_000
-        // But 9빼고 모든 버튼이 고장날 경우 999_999도 가능
-        int minPushCnt = Math.abs(N - 100);
-        for (int channel = 0; channel <= 999_999; channel++) {
-            String strChannel = Integer.toString(channel);
-            if (!checkChannel(strChannel, remoteController))
-                continue;
+		int min = Math.abs(100-N);
 
-            int len = strChannel.length();
-            minPushCnt = Math.min(minPushCnt, len + Math.abs(N - channel));
-        }
-        System.out.print(minPushCnt);
-    }
-    public static boolean checkChannel(String strChannel, boolean[] remoteController) {
-        for (int c = 0; c < strChannel.length(); c++)
-            if(!remoteController[strChannel.charAt(c) - '0'])
-                return false;
-        return true;
-    }
+		chanel:
+		for(int i = 0; i<=999999; i++) {
+			int cnt = 0;
+			char[] chanel = String.valueOf(i).toCharArray();
+			for (char c : chanel)
+				if (brokenButton[c - '0'])
+					continue chanel;
+
+			cnt = chanel.length + Math.abs(i-N);
+			min = Math.min(cnt, min);
+		}
+		System.out.println(min);
+	}
 }
