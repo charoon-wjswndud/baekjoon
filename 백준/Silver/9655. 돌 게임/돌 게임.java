d55e6abaa643445123d330461d5a812d4f70fdcd
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-09-26 17:30:55 +0900

    [Silver V] Title: 돌 게임, Time: 76 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -1,16 +1,21 @@
-import java.util.Scanner;
-
+import java.util.*;
+import java.io.*;
 public class Main {
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-
-		int[] dp = new int[1001];
-		dp[1] = 1;
-		dp[2] = 2;
-		dp[3] = 1;
-		for (int i = 4; i <= n; i++)
-			dp[i] = Math.min(dp[i-1], dp[i-3]) + 1;
-		System.out.print(dp[n] % 2 == 0 ? "CY" : "SK");
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 1;
+        
+        for (int i = 4; i <= n; i++) 
+            dp[i] = Math.min(dp[i-1],dp[i-3]) + 1;
+        
+        if (dp[n] % 2 == 0)
+            System.out.print("CY");
+        else
+            System.out.print("SK");
+    }
 }
