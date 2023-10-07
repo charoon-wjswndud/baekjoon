646bbd28d23469c00d40f08ddbb05000903b1097
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-10-07 19:40:55 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 80 ms, Memory: 11624 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -3,17 +3,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		for (int n = 0; n < N; n++) {
-			for (int start = 0; start <= n; start++) {
-				sb.append("*");
-			}
-			sb.append("\n");
-		}
 
-		System.out.print(sb);
-	}
+    static int minDistance = Integer.MAX_VALUE;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++)
+                sb.append("*");
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
 }
