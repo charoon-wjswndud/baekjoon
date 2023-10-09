58070b07a6f9058386e0d861b9ab401566768edf
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-10-09 20:35:12 +0900

    [Bronze V] Title: 구구단, Time: 80 ms, Memory: 11436 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,12 +1,17 @@
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		
-		for (int i = 1; i <= 9; i++) {
-			System.out.printf("%d * %d = %d\n", num, i, num*i);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());	
+		StringBuilder sb = new StringBuilder();
+        
+		for(int i = 1; i<10;i++) {
+			sb.append(num).append(' ').append('*').append(' ').append(i);
+			sb.append(' ').append('=').append(' ').append(num*i).append('\n');
 		}
+		System.out.print(sb);
 	}
 }
