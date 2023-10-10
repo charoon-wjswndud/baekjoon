fb50d404889c752666d98436e07b758ca2cf08a2
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-10-10 22:04:54 +0900

    [Bronze V] Title: 시험 성적, Time: 80 ms, Memory: 11484 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,25 +1,21 @@
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int score = sc.nextInt();
-		switch (score/10) {
-			case 9: case 10:
-				System.out.println('A');
-				break;
-			case 8:
-				System.out.println('B');
-				break;
-			case 7:
-				System.out.println('C');
-				break;
-			case 6:
-				System.out.println('D');
-				break;
-			default:
-				System.out.println('F');
-				break;
-		}
+ 	public static void main(String[] args) throws IOException {        
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 		int A = Integer.parseInt(br.readLine());
+        
+		if(A>=90) 
+            System.out.println("A");
+		else if(A>=80) 
+            System.out.println("B");
+		else if (A>=70) 
+            System.out.println("C");
+		else if(A>=60) 
+            System.out.println("D");
+		else 
+            System.out.println("F");
 	}
 }
