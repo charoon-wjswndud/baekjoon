506561b991278ba3b1f3f60ae691f9f27593d619
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-10-13 21:41:14 +0900

    [Bronze V] Title: A+B - 3, Time: 132 ms, Memory: 13080 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 public class Main {
 	public static void main(String args[]) {
-		Scanner in = new Scanner(System.in);
-		int c = in.nextInt();
-		int arr[] = new int[c];
-		for (int i = 0; i < c; i++) {
-			int a = in.nextInt();
-			int b = in.nextInt();
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		int arr[] = new int[t];
+
+		for (int i = 0; i < t; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
 			arr[i] = a + b;
 		}
-		for (int k : arr) 
+		for (int k : arr)
 			System.out.println(k);
 	}
- 
 }
