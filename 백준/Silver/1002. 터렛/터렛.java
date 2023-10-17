ec9dc61260dab80842410da2db1e627485f69838
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-10-17 17:25:12 +0900

    [Silver III] Title: 터렛, Time: 80 ms, Memory: 11812 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.java
+++ 백준/Silver/1002. 터렛/터렛.java
@@ -1,36 +1,43 @@
-import java.awt.Point;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        while (0 < T--) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		
-		for (int testCase = 0; testCase < T; testCase++) {
-			
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			Point jo = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-			int joDistance = Integer.parseInt(st.nextToken());
-			Point back = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-			int backDistance = Integer.parseInt(st.nextToken());
-			
-			sb.append(solution(jo, joDistance, back, backDistance) +"\n");
-		}
-		System.out.println(sb);
-	}
-	
-	private static int solution(Point jo, int joDistance, Point back, int backDistance) {
-		int distance_pow = (int)(Math.pow(back.x - jo.x, 2) + Math.pow(back.y - jo.y, 2));	
-		if(back.x == jo.x && back.y == jo.y && joDistance == backDistance) return -1;
-		else if(distance_pow > Math.pow(backDistance + joDistance, 2)) return 0;
-		else if(distance_pow < Math.pow(backDistance - joDistance, 2)) return 0;
-		else if(distance_pow == Math.pow(backDistance - joDistance, 2)) return 1;
-		else if(distance_pow == Math.pow(backDistance + joDistance , 2)) return 1;
-		else return 2;
-	}
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int r1 = Integer.parseInt(st.nextToken());
+
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            int r2 = Integer.parseInt(st.nextToken());
+
+            int result = tangent_point(x1, y1, r1, x2, y2, r2);
+            sb.append(result).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+    public static int tangent_point(int x1, int y1, int r1, int x2, int y2, int r2) {
+        int distance_pow = (int) (Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        if(x1 == x2 && y1 == y2 && r1 == r2)
+            return -1;
+        else if(distance_pow > Math.pow(r1 + r2, 2))
+            return 0;
+        else if(distance_pow < Math.pow(r2 - r1, 2))
+            return 0;
+        else if(distance_pow == Math.pow(r2 - r1, 2))
+            return 1;
+        else if(distance_pow == Math.pow(r1 + r2, 2))
+            return 1;
+        else
+            return 2;
+    }
 }
