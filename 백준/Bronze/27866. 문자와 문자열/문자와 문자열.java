7af88286733662d57f81abac1c412369d50282ce
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-10-30 19:33:36 +0900

    [Bronze V] Title: 문자와 문자열, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -1,10 +1,11 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder(br.readLine());
-		System.out.print(sb.charAt(Integer.parseInt(br.readLine())-1));
+		String s = br.readLine();
+		int i = Integer.parseInt(br.readLine())-1;
+		System.out.println(s.charAt(i));
 	}
 }
