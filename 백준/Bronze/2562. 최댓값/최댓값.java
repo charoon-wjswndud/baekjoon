ca2a6f2162d28743347d54609d1f1e406eef82d0
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-11-03 20:18:01 +0900

    [Bronze III] Title: 최댓값, Time: 76 ms, Memory: 11480 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,19 +1,23 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
- 
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int max = 0;
-		int idx = 0;
-		for(int i = 0 ; i < 9 ; i++) {
-			int val = Integer.parseInt(br.readLine());
-			if(val > max) {
-				max = val;
-				idx = i+1;
-			}
-		}
-		System.out.print(max + "\n" + idx);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int maxNum = Integer.parseInt(br.readLine());
+        int maxCnt = 1;
+        for (int i = 2; i <= 9; i++) {
+            int num  = Integer.parseInt(br.readLine());
+            if (num > maxNum) {
+                maxNum = num;
+                maxCnt = i;
+            }
+        }
+
+        System.out.println(maxNum);
+        System.out.println(maxCnt);
+    }
 }
