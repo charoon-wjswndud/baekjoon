86e08df3cf75a7d068f73046ad14d1293502b8f8
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-11-14 18:27:47 +0900

    [Bronze I] Title: 단어 공부, Time: 184 ms, Memory: 20504 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[26];
+        String line = br.readLine();
+
+        for (int i = 0; i < line.length(); i++) 
+            if ('a' <= line.charAt(i) && line.charAt(i) <= 'z') 
+                arr[line.charAt(i) - 97]++;
+            else 
+                arr[line.charAt(i) - 65]++;
+        
+        int max = -1;
+        char ch = '?';
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                ch = (char) (i + 65);
+            }
+            else if (arr[i] == max) 
+                ch = '?';
+        }
+        System.out.print(ch);
+    }
+}
