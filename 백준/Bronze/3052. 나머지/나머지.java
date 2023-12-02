8150bff42bb341ba5fb1141da08ece2352d9313c
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-12-02 15:50:02 +0900

    [Bronze II] Title: 나머지, Time: 76 ms, Memory: 11484 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,13 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.HashSet;
- 
+import java.util.Set;
+
 public class Main {
- 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		HashSet<Integer> hash = new HashSet<Integer>();
-		for (int i = 0; i < 10; i++)
-			hash.add(sc.nextInt() % 42);
-		System.out.print(hash.size());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Set<Integer> set = new HashSet<>();
+
+		for (int i = 0; i < 10; i++) {
+			int num = Integer.parseInt(br.readLine()) % 42;
+			set.add(num);
+		}
+
+		System.out.println(set.size());
 	}
 }
