e4c4cce8e855add68a2cf52f85a2c92b1af3c2c0
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-12-07 17:47:28 +0900

    [Bronze V] Title: 나머지, Time: 76 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,16 +1,23 @@
-import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
 public class Main {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		int C = sc.nextInt();
-		
-		System.out.println((A+B)%C);
-		System.out.println(((A%C) + (B%C))%C);
-		System.out.println((A*B)%C);
-		System.out.println(((A%C)*(B%C))%C);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        sb.append((A+B)%C).append("\n");
+        sb.append(((A % C) + (B % C)) % C).append("\n");
+        sb.append((A * B) % C).append("\n");
+        sb.append(((A % C) * (B % C)) % C).append("\n");
+
+        System.out.print(sb);
+    }
 }
