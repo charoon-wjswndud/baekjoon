0d2824299bb3890aac3453b5593db6af147f729a
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-12-28 16:13:42 +0900

    [Gold IV] Title: 알파벳, Time: 1132 ms, Memory: 13472 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,48 +1,55 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
-	static final int[] dx = {1, 0, -1, 0};
-	static final int[] dy = {0, 1, 0, -1};
-	static int maxCount = 0;
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	static int R, C, maxCnt;
+	static boolean[][] visited;
+	static char[][] map;
+	static int[][] dr = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int R = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
-		
-		char[][] arr = new char[R][C];
-		boolean[] checkAlphabet = new boolean['Z'-('A'-1)]; 
-		for (int i = 0; i < R; i++) {
-			arr[i] = br.readLine().toCharArray();
+
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		map = new char[R][C];
+
+		for (int r = 0; r < R; r++) {
+			char[] line = br.readLine().toCharArray();
+			System.arraycopy(line, 0, map[r], 0, C);
 		}
-		dfs(new Position(0, 0), arr, checkAlphabet, 0);
-		System.out.println(maxCount);
+
+		maxCnt = 1;
+
+		visited = new boolean[R][C];
+		visited[0][0] = true;
+
+		boolean[] check = new boolean[26];
+		check[map[0][0] - 'A'] = true;
+
+		dfs(0, 0, 1, check);
+		System.out.println(maxCnt);
 	}
-	private static void dfs(Position position, char[][] arr, boolean[] checkAlphabet, int count) {
-		if(checkAlphabet[arr[position.row][position.column]-'A']) {
-			maxCount = Math.max(count, maxCount);
-			return;
+
+	private static void dfs(int r, int c, int cnt, boolean[] check) {
+		for (int[] nd:
+			 dr) {
+			int nr = r + nd[0];
+			int nc = c + nd[1];
+			if (nr < 0 || R <= nr || nc < 0 || C <= nc)
+				continue;
+			if (visited[nr][nc])
+				continue;
+			if (check[map[nr][nc] - 'A'])
+				continue;
+			visited[nr][nc] = true;
+			check[map[nr][nc] - 'A'] = true;
+			dfs(nr, nc, cnt+1, check);
+			visited[nr][nc] = false;
+			check[map[nr][nc] - 'A'] = false;
 		}
-		checkAlphabet[arr[position.row][position.column]-'A'] = true;
-		for (int i = 0; i < 4; i++) {
-			int x = position.column + dx[i];
-			int y = position.row + dy[i];
-			if((0 <= x) && (x < arr[0].length) && (0 <= y) && (y < arr.length)) {
-				dfs(new Position(y, x), arr, checkAlphabet, count+1);
-			}
-		}
-		checkAlphabet[arr[position.row][position.column]-'A'] = false;
-	}
-	static class Position{
-		int row;
-		int column;
-		public Position(int row, int column) {
-			super();
-			this.row = row;
-			this.column = column;
-		}
+		maxCnt = Math.max(maxCnt, cnt);
 	}
 }
