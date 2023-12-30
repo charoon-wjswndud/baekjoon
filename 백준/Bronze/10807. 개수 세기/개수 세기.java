394ef5138923bb9264338eb6edacb17a7b1ec162
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2023-12-30 17:18:25 +0900

    [Bronze V] Title: 개수 세기, Time: 76 ms, Memory: 11600 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,34 +1,22 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.IOException;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int n = Integer.parseInt(br.readLine());
-        int[] num = new int[n];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 
-        int v = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			map.put(num, map.getOrDefault(num, 0)+1);
+		}
 
-        int answer = 0;
-
-        for(int i = 0; i < n; i++) {
-
-            num[i] = Integer.parseInt(st.nextToken());
-
-            if(num[i] == v) {
-
-                answer++;
-
-            }
-
-        }
-
-        System.out.println(answer);
-
-    }
-
+		int num = Integer.parseInt(br.readLine());
+		System.out.println(map.getOrDefault(num, 0));
+	}
 }
