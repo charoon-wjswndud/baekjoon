79dad6d62e38354a0e560ee10e4f92dd40966d24
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2024-01-05 19:53:28 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 76 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,17 +1,24 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int[] student = new int[31];
-		for(int i=1; i<29; i++) {
-			int success = sc.nextInt();
-			student[success] = 1;
-		}
-		for(int i=1; i<student.length; i++) {
-			if(student[i]!=1)
-				System.out.println(i);
-		}
-	}
+    static Set<String> set = new HashSet<>();
+    static String[][] map = new String[5][5];
+    static int[][] direction = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        boolean[] arr= new boolean[31];
+
+        for (int i = 0; i < 28; i++) {
+            int num = Integer.parseInt(br.readLine());
+            arr[num] = true;
+        }
+
+        for (int i = 1; i <= 30; i++) {
+            if (arr[i])
+                continue;
+            System.out.println(i);
+        }
+    }
 }
