51c6b1cf71aabed80f8917ba7f5170e86cba11ac
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2024-01-13 17:12:29 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 120 ms, Memory: 13220 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,40 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    StringTokenizer st  = new StringTokenizer(br.readLine());
-	    int N = Integer.parseInt(st.nextToken());
-	    int M = Integer.parseInt(st.nextToken());
-	    
-	    int[][] A = new int[N][M];
-	    int[][] B = new int[N][M];
-	    
-	    for(int i =0 ; i < N; i++) {
-	    	st = new StringTokenizer(br.readLine());
-	    	for(int j = 0; j < M; j++) {
-	    		A[i][j] = Integer.parseInt(st.nextToken());
-	    	}
-	    }
-	    
-	    for(int i =0 ; i < N; i++) {
-	    	st = new StringTokenizer(br.readLine());
-	    	for(int j = 0; j < M; j++) {
-	    		B[i][j] = Integer.parseInt(st.nextToken());
-	    	}
-	    }
-	    
-	    StringBuilder sb = new StringBuilder();
-	    for(int i =0 ; i < N; i++) {
-	    	for(int j = 0; j < M; j++) {
-	    		sb.append((A[i][j] + B[i][j]) + " ");
-	    	}
-	    	sb.append("\n");
-	    }
-	    
-	    System.out.println(sb.toString());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[][] arr = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < m; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < m; j++) {
+				sb.append(arr[i][j] + Integer.parseInt(st.nextToken())).append(" ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
 	}
 }
