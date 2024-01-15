0973752e9621d98e0823bee69eee924ceda9b07f
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2024-01-15 20:19:09 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 76 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -1,9 +1,9 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print(br.readLine().length());
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(br.readLine().length());
+    }
 }
