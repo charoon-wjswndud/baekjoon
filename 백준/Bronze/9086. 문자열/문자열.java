f5f9cb194dadcc18098c6248a8377ad1d4515333
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2024-01-17 20:44:52 +0900

    [Bronze V] Title: 문자열, Time: 76 ms, Memory: 11300 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,15 +1,18 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int tc = Integer.parseInt(br.readLine());
+
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            String s = br.readLine();
-            sb.append(s.charAt(0));
-            sb.append(s.charAt(s.length()- 1) + "\n");
+        while (tc-- > 0) {
+            char[] string = br.readLine().toCharArray();
+            sb.append(string[0]).append(string[string.length - 1]).append("\n");
         }
+
         System.out.print(sb);
     }
 }
