4fa0047109f225f7bfa109d60acdd4fe5e3c7e23
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2024-02-04 19:42:53 +0900

    [Silver IV] Title: 문자열, Time: 80 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Silver/1120. 문자열/문자열.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String A = st.nextToken();
+        String B = st.nextToken();
+
+        int ans = A.length();
+
+        for (int i = 0; i <= B.length() - A.length(); i++) {
+            int cnt = 0;
+            for (int j = 0; j < A.length(); j++) {
+                if (A.charAt(j) != B.charAt(i + j))
+                    cnt++;
+            }
+            ans = Math.min(cnt, ans);
+        }
+        System.out.println(ans);
+    }
+}
