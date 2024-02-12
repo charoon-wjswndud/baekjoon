036490db2cfd6a49331c2853dbbd966e20aca56d
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2024-02-12 22:44:35 +0900

    [Bronze V] Title: 긴자리 계산, Time: 96 ms, Memory: 12628 KB -BaekjoonHub

--- 백준/Bronze/2338. 긴자리 계산/긴자리 계산.java
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.java
@@ -3,14 +3,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
 
-class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BigInteger A = new BigInteger(br.readLine());
-		BigInteger B = new BigInteger(br.readLine());
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger first = new BigInteger(br.readLine());
+        BigInteger second = new BigInteger(br.readLine());
 
-		StringBuilder sb = new StringBuilder();
-		sb.append(A.add(B)).append("\n").append(A.subtract(B)).append("\n").append(A.multiply(B));
-		System.out.print(sb);
-	}
+        System.out.println(first.add(second));
+        System.out.println(first.subtract(second));
+        System.out.print(first.multiply(second));
+
+    }
 }
