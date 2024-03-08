7f4a67c413277ab532804c0b64b931fafd256f9a
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2024-03-08 21:16:18 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 76 ms, Memory: 11480 KB -BaekjoonHub

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -1,11 +1,12 @@
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
 public class Main {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int year = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		System.out.println(year-543);
+		int year = Integer.parseInt(br.readLine());
+		System.out.println(year - 543);
 	}
 }
