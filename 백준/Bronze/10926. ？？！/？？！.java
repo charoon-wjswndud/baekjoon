5d0fd879236263c6ce66b3cd602754a2b2733220
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2024-03-09 22:49:27 +0900

    [Bronze V] Title: ??!, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,11 +1,8 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		String name = sc.nextLine();
-		
-		System.out.println(name.concat("??!"));
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine() + "??!");
+    }
 }
