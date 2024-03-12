a7414e9940d2751c6cfd58aa941159252a86ece0
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2024-03-12 23:24:11 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 124 ms, Memory: 13128 KB -BaekjoonHub

--- 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -1,17 +1,13 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigInteger;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		BigInteger n = new BigInteger(st.nextToken());
-		BigInteger m = new BigInteger(st.nextToken());
-		StringBuilder sb = new StringBuilder();
-		sb.append(n.divide(m)).append("\n").append(n.remainder(m));
-		System.out.print(sb);
-	}
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BigInteger n = in.nextBigInteger();
+        BigInteger m = in.nextBigInteger();
+        System.out.println(n.divide(m));
+        System.out.println(n.remainder(m));
+
+    }
 }
