0128e1dad2a11049341c81e5061e572358899025
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2024-03-19 22:44:16 +0900

    [Gold IV] Title: N-Queen, Time: 5768 ms, Memory: 12076 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,40 +1,42 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
-	public static int[] arr;
-	public static int N;
-	public static int count = 0;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		arr = new int[N];
+    static int[] arr;
+    static int N;
+    static int cnt;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		nQueen(0);
-		System.out.println(count);
-	}
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        cnt = 0;
 
-	public static void nQueen(int depth) {
-		if (depth == N) {
-			count++;
-			return;
-		}
-		for (int i = 0; i < N; i++) {
-			arr[depth] = i;
-			if (Possibility(depth))
-				nQueen(depth + 1);
-		}
-	}
+        queen(0);
+        System.out.println(cnt);
+    }
 
-	public static boolean Possibility(int col) {
-		for (int i = 0; i < col; i++) {
-			if (arr[col] == arr[i]) {
-				return false;
-			}else if (Math.abs(col - i) == Math.abs(arr[col] - arr[i])) {
-				return false;
-			}
-		}
-		return true;
-	}
+    private static void queen(int depth) {
+        if (depth == N) {
+            cnt++;
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            arr[depth] = i;
+            if (check(depth))
+                queen(depth+1);
+        }
+    }
+
+    private static boolean check(int depth) {
+        for (int i = 0; i < depth; i++) {
+            if (arr[depth] == arr[i])
+                return false;
+            if (Math.abs(depth - i) == Math.abs(arr[depth] - arr[i]))
+                return false;
+        }
+
+        return true;
+    }
+
 }
