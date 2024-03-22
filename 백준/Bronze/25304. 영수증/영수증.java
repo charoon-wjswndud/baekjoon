6b6ba0fcc887ac859f9964d2197bc8811632c4a5
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2024-03-22 23:06:05 +0900

    [Bronze IV] Title: 영수증, Time: 128 ms, Memory: 13040 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,15 +1,22 @@
 import java.util.Scanner;
 
-interface Main {
-    public static void main (String[] args) {
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int X = sc.nextInt(), N = sc.nextInt(), sum = 0;
+        int X = sc.nextInt(); 
+        int N = sc.nextInt(); 
+        int total = 0;
 
-        for(int i=0; i<N; i++) 
-            sum += sc.nextInt() * sc.nextInt();
-        if(X == sum)
+        for(int i = 0; i < N; i++){
+            int a = sc.nextInt(); 
+            int b = sc.nextInt(); 
+            total += a * b;
+        }
+        
+        if(total == X)
             System.out.println("Yes");
         else
             System.out.println("No");
+        
     }
 }
