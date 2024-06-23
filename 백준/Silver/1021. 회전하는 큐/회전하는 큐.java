60a3031a47aa0479b2c5730c9fdd5d1f4f6c0487
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2024-06-23 22:05:44 +0900

    [Silver III] Title: 회전하는 큐, Time: 76 ms, Memory: 11640 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -1,50 +1,62 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static Deque<Integer> deque;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        LinkedList<Integer> deque = new LinkedList<Integer>();
+        st = new StringTokenizer(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        for(int i = 1; i <= N; i++)
-            deque.offer(i);
-
-        int[] arr = new int[M];
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < M; i++)
-            arr[i] = Integer.parseInt(st.nextToken());
+        int[] set = new int[M];
+        for (int i = 0; i < M; i++)
+            set[i] = Integer.parseInt(st.nextToken());
+
+        deque = new LinkedList<>();
+        for (int i = 1; i <= N; i++)
+            deque.addLast(i);
 
         int cnt = 0;
-        for(int i = 0; i < M; i++) {
-            int targetIdx = deque.indexOf(arr[i]);
-            int halfIdx;
+        for (int num : set) {
+            if (deque.peekFirst() == num) {
+                deque.removeFirst();
+                continue;
+            }
 
-            if(deque.size() % 2 == 0)
-                halfIdx = deque.size() / 2 - 1;
+            int targetIndex = ((LinkedList) deque).indexOf(num);
+            int halfIndex;
+            if (deque.size()%2 == 1)
+                halfIndex = deque.size()/2;
             else
-                halfIdx = deque.size() / 2;
+                halfIndex = deque.size()/2-1;
 
-            if(targetIdx <= halfIdx) {
-                for(int j = 0; j < targetIdx; j++) {
-                    deque.offerLast(deque.pollFirst());
+            if (halfIndex >= targetIndex) {
+                while (num != deque.peekFirst()) {
+                    moveLeft();
                     cnt++;
                 }
             } else {
-                for(int j = 0; j < deque.size() - targetIdx; j++) {
-                    deque.offerFirst(deque.pollLast());
+                while (num != deque.peekFirst()) {
+                    moveRight();
                     cnt++;
                 }
             }
-            deque.pollFirst();
+            deque.removeFirst();
         }
+
         System.out.println(cnt);
+    }
+
+    private static void moveLeft() {
+        deque.addLast(deque.removeFirst());
+    }
+
+    private static void moveRight() {
+        deque.addFirst(deque.removeLast());
     }
 }
